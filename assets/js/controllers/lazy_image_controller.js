2a233215cb053d44dfecb378d0f191da348cde6c
Maxime Steinhausser <maxime.steinhausser@elao.com>
2021-04-18 15:24:18 +0200

    Optimize img order loading & reduce some resized images quality

--- assets/js/controllers/lazy_image_controller.js
+++ assets/js/controllers/lazy_image_controller.js
@@ -4,38 +4,54 @@ import { Controller } from 'stimulus';
 
 /**
  * Same as original Symfony UX controller, but handling srcSet attributes.
+ * Also prevents loading all images at once. Loads in call order and wait instead.
  *
  * @see https://github.com/symfony/ux-lazy-image/blob/main/Resources/assets/src/controller.js
  */
 export default class extends Controller {
+  static ready = true;
+
   connect() {
-    const hd = new Image();
-
-    hd.addEventListener('load', () => {
-      this.element.src = this.element.getAttribute('data-hd-src');
-      if (this.element.getAttribute('data-hd-srcset')) {
-        this.element.srcset = this.element.getAttribute('data-hd-srcset');
-      }
-
-      // Remove the tmp width attribute asked
-      if (this.element.getAttribute('data-hd-add-width')) {
-        delete this.element.removeAttribute('width');
-      }
-
-      this._dispatchEvent('lazy-image:ready', { hd });
-    });
-
-    hd.src = this.element.getAttribute('data-hd-src');
-    if (this.element.getAttribute('data-hd-srcset')) {
-      hd.srcset = this.element.getAttribute('data-hd-srcset');
-    }
-
     // Add a tmp width attribute if asked:
     if (this.element.getAttribute('data-hd-add-width')) {
       this.element.width = this.element.getAttribute('data-hd-add-width');
     }
 
-    this._dispatchEvent('lazy-image:connect', { hd });
+    const interval = setInterval(() => {
+      if (!this.constructor.ready) {
+        return;
+      }
+
+      // stop waiting for previous image to be loaded:
+      clearInterval(interval);
+      this.constructor.ready = false;
+
+      const hd = new Image();
+
+      hd.addEventListener('load', () => {
+        this.element.src = this.element.getAttribute('data-hd-src');
+        if (this.element.getAttribute('data-hd-srcset')) {
+          this.element.srcset = this.element.getAttribute('data-hd-srcset');
+        }
+
+        // Remove the tmp width attribute asked
+        if (this.element.getAttribute('data-hd-add-width')) {
+          delete this.element.removeAttribute('width');
+        }
+
+        this._dispatchEvent('lazy-image:ready', { hd });
+
+        // Let the next image load:
+        this.constructor.ready = true;
+      });
+
+      hd.src = this.element.getAttribute('data-hd-src');
+      if (this.element.getAttribute('data-hd-srcset')) {
+        hd.srcset = this.element.getAttribute('data-hd-srcset');
+      }
+
+      this._dispatchEvent('lazy-image:connect', { hd });
+    }, 50);
   }
 
   _dispatchEvent(name, payload = null, canBubble = false, cancelable = false) {
